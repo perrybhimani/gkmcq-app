b7fb7407932d859cce993df91b6cb978546c38c9
perry <perrybhimani1818@gmail.com>
2022-09-10 21:55:45 +0530

    change

--- app/redux/Api.js
+++ app/redux/Api.js
@@ -21,7 +21,6 @@ import {
 import { getToken, setToken } from "../utils/storageUtils";
 import { common } from "../constants/strings";
 import moment from "moment";
-import setAuthToken from '../utils/setAuthToken';
 
 axios.defaults.baseURL = BASE_URL;
 
@@ -32,7 +31,6 @@ export async function signUp(data) {
       const responseData = idx(res, (_) => _.data.data) || {};
       const status = idx(res, (_) => _.status);
       const userToken = idx(responseData, (_) => _.token) || '';
-      setAuthToken(userToken);
       setToken(userToken);
       return { status, data: responseData };
     })
